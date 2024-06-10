8ed50b8e25ce00356a53f2d3c226ab44e6048be9
ParkYongHo1 <qkaxhf8823@naver.com>
2024-06-10 09:41:55 +0900

    props test

--- management/src/main/frontend/src/atoms/buttons/Button.js
+++ management/src/main/frontend/src/atoms/buttons/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-const Button = ({ children, onClick }) => {
+const Button = ({ children, onClick }) => {  
     return <StyledButton onClick={onClick}>{children}</StyledButton>;
 };
 
