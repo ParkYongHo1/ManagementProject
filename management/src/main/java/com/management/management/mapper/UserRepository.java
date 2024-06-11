99a5745373306039a46c8942302430f4398b8efd
Kim Dong Hui <ehdgnl971000@kakao.com>
2024-06-11 11:23:19 +0900

    feat:로그인 기능 구현

--- management/src/main/java/com/management/management/mapper/UserRepository.java
+++ management/src/main/java/com/management/management/mapper/UserRepository.java
@@ -2,9 +2,17 @@ package com.management.management.mapper;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
 import com.management.management.DTO.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Optional -> null값이 있을 수 있을때 사용
+    
+    @Query("SELECT s FROM User s WHERE s.userId =?1")
     Optional<User> findByUserId(String userId);
+
+    @Query("SELECT s FROM User s WHERE s.userPassword =?1")
+    Optional<User> findByUserPassword(String userPassword);
 }
