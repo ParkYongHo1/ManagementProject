2fcbcb7949a2fa25574648b108afdf7706c7ea69
Kim Dong Hui <ehdgnl971000@kakao.com>
2024-06-17 22:48:47 +0900

    :card_file_box: 데이버베이스 관련 수정

---
+++ management/src/main/sql/store.sql
@@ -0,0 +1,6 @@
+CREATE TABLE store (
+    store_code VARCHAR(100) NOT NULL PRIMARY KEY,
+    store_name VARCHAR(100) NOT NULL,
+    store_address VARCHAR(100) NOT NULL,
+    store_tel VARCHAR(100) NOT NULL
+);
